6a17ed52b8c2d814a740254ebc195d693995366a
jhonathancs <83834773+jhonathancs@users.noreply.github.com>
2021-10-08 06:53:51 -0400

    Ajustes finais
    Finish

---
+++ Pokedex-React-master/src/App.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect, ReactDOM } from 'react';
+import Navbar from './components/Navbar';
+import Card from './components/Card';
+import { getPokemon, getAllPokemon } from './services/pokemon';
+import './App.css';
+import myimage from './my-image.gif'
+
+
+function App() {
+  const [pokemonData, setPokemonData] = useState([])
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialURL = 'https://pokeapi.co/api/v2/pokemon'
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialURL)
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, [])
+
+  const next = async () => {
+    setLoading(true);
+    let data = await getAllPokemon(nextUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const prev = async () => {
+    if (!prevUrl) return;
+    setLoading(true);
+    let data = await getAllPokemon(prevUrl);
+    await loadPokemon(data.results);
+    setNextUrl(data.next);
+    setPrevUrl(data.previous);
+    setLoading(false);
+  }
+
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(data.map(async pokemon => {
+      let pokemonRecord = await getPokemon(pokemon)
+      return pokemonRecord
+    }))
+    setPokemonData(_pokemonData);
+  }
+  
+  return (
+    <>
+      <Navbar />
+      <div>
+        {loading ? <h1 style={{ textAlign: 'center' }}>Loading...</h1> : (
+          <>
+            <div  className = "nes-container is-rounded" id = "conteiner">
+                <div>
+                  <p id = "firstP">Bem vindo a Pokedex!! <br></br>Está é uma api em que se obtem
+                  <br></br> todos os dados sobre todos pokemons!</p>
+                </div>
+                <div id = 'hello'>
+                  <p className = "animate__slideInLeft">Hello World!!</p>
+                </div>
+                <div>
+                  <img src = {myimage} className = "nes-container is-rounded" id = "photo"></img>
+                </div>
+              
+                <div id = "conteinerTwo">
+                <button >Click Aqui</button>
+                </div>
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+            <div className="grid-container">
+              {pokemonData.map((pokemon, i) => {
+                return <Card key={i} pokemon={pokemon} />
+              })}
+            </div>
+            <div className="btn">
+              <button onClick={prev}>Prev</button>
+              <button onClick={next}>Next</button>
+            </div>
+          </>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
